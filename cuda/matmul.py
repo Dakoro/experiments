31f95153a82e79500e222715a65528aad40ae461
Dakoro <david.korobetski@gmail.com>
2024-08-09 21:32:22 +0200

    refactor: update cuda experiment

--- cuda/matmul.py
+++ cuda/matmul.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import timeit
 
@@ -7,11 +8,12 @@ def matmul(A, B):
 
 
 def main():
-    N = [128, 256, 1024]
+    N = [128, 256, 1024, 4096]
     for n in N:
         x = np.random.randn(n, n).astype(np.float32)
         y = np.random.randn(n, n).astype(np.float32)
-        print(timeit.timeit(lambda: matmul(x, y), number=10000, ))
+        print(timeit.timeit(lambda: matmul(x, y), number=10000))
 if __name__ == '__main__':
+    print(f"PID: {os.getpid()}")
     main()
     
--- tools/sotrace.py
+++ tools/sotrace.py
@@ -14,117 +14,132 @@
 
 import os	# For popen
 import sys	# For argv
-
+import fire
+import subprocess
+import shlex
+import loguru as log
 
 # Given a target library name, see what this library directly depends on.
-def dep_list(tgt) :
-	cmd = "readelf -d " + tgt + " | grep NEEDED"
-	f = os.popen(cmd, 'r')
-	lines = f.readlines()
-	f.close()
-	vals = [ x.split(": ")[1].strip() for x in lines ]
-	deps = [ x[1:-1] for x in vals ]
-	return deps
+def dep_list(target) :
+    obj_cmd = ["readelf",  "-d", target]
+    grep_cmd = ["grep", "NEEDED"]
+    try:
+        lines = subprocess.check_output(
+			shlex.join(obj_cmd) + "|" + shlex.join(grep_cmd),
+			shell=True).decode('utf-8')
+        vals = [ x.split()[-1].strip() for x in lines.split('\n') if len(x.split()) > 1]
+        deps = [ x[1:-1] for x in vals ]
+        return deps
+    except subprocess.CalledProcessError:
+        log.logger.error("Procedd call failed")
+        
+        
+     
 
 
 # Given a set of dependency names, check to what path the are resolved using ldd
-def dep_to_lib(tgt, deps) :
-	cmd = "ldd " + tgt
-	f = os.popen(cmd, 'r')
-	lines = f.readlines()
-	f.close()
-	mapping = {}
-	for line in lines:
-		if "=>" in line:
-			parts = line.strip().split(" => ")
-			nam = parts[0].strip()
-			if nam in deps :
-				mapping[nam] = parts[1].split(" (")[0]
-	return mapping
+def dep_to_lib(target, deps) :
+    cmd = ["ldd", target]
+    try:
+        lines = subprocess.check_output(shlex.join(cmd), shell=True).decode("utf-8")
+        mapping = {}
+        for line in lines:
+            if "=>" in line:
+                parts = line.strip().split(" => ")
+                nam = parts[0].strip()
+                if nam in deps :
+                    mapping[nam] = parts[1].split(" (")[0]
+        return mapping
+    except subprocess.CalledProcessError:
+        log.logger.error("Procedd called failed")
 
 
 # Walk the dependencies of the target, and write the graph to file.
-def traverse_so(tgt, nam, f, depth, visited, linked, keep_suffix) :
-	visited.add(tgt)
-	deps = dep_list(tgt)
-	lib_map = dep_to_lib(tgt, deps)
-
-	for val in lib_map.keys() :
-		link = (nam, val) if keep_suffix else (nam.split('.so')[0], val.split('.so')[0])
-		linked.add(link)
-
-	for dep in deps:
-		if dep in lib_map:
-			m = lib_map[dep]
-			if m not in visited:
-				visited.add(m)
-				dnam = os.path.basename(dep)
-				traverse_so(m, dnam, f, depth+1, visited, linked, keep_suffix)
+def traverse_so(target, nam, f, depth, visited, linked, keep_suffix) :
+    visited.add(target)
+    deps = dep_list(target)
+    lib_map = dep_to_lib(target, deps)
+    if isinstance(lib_map, dict):
+        for val in lib_map.keys() :
+            link = (nam, val) if keep_suffix else (nam.split('.so')[0], val.split('.so')[0])
+            linked.add(link)
+            for dep in deps:
+                if dep in lib_map:
+                    m = lib_map[dep]
+                    if m not in visited:
+                        visited.add(m)
+                        dnam = os.path.basename(dep)
+                        traverse_so(m, dnam, f, depth+1, visited, linked, keep_suffix)
 
 
 # Walk the deps, starting from the mapped files of a process.
-def trace_pid(tgt, f, visited, linked, keep_suffix) :
-	nf = open("/proc/%s/comm" % (tgt,), "r")
-	nam = nf.readline().strip()
-	nf.close()
-	cmd = "ls -l /proc/%s/map_files" % (tgt,)
-	cf = os.popen(cmd, "r")
-	lines = [ x.split(" -> ")[1].strip() for x in cf.readlines() if " -> " in x and ".so" in x ]
-	cf.close()
-	libs = sorted(set(lines))
-	print("Tracing shared objects from command", nam, "with", len(lines), "mapped .so files.")
-	lib_map = {}
+def trace_pid(target, f, visited, linked, keep_suffix) :
+    with open(f"/proc/{target}/comm", "r") as nf:
+	    nam = nf.readline().strip()
+    cmd = ['ls', "-l", f"/proc/{target}/map_files"]
+    cf = subprocess.check_output(shlex.join(cmd), shell=True).decode('utf-8')
+    lines = [ x.split(" -> ")[1].strip() for x in cf.split('\n') if " -> " in x and ".so" in x ]
+    libs = sorted(set(lines))
+    print("Tracing shared objects from command", nam, "with", len(lines), "mapped .so files.")
+    lib_map = {}
 
-	depth = 0
+    depth = 0
 
-	for lib in libs:
-		libname = os.path.basename(lib)
-		lib_map[libname] = lib
+    for lib in libs:
+        libname = os.path.basename(lib)
+        lib_map[libname] = lib
 
-	for val in lib_map.keys() :
-		if keep_suffix :
-			link = (nam, val)
-			linked.add(link)
-		else :
-			link = (nam.split('.so')[0], val.split('.so')[0])
-			linked.add(link)
+    for val in lib_map.keys() :
+        if keep_suffix :
+            link = (nam, val)
+            linked.add(link)
+        else :
+            link = (nam.split('.so')[0], val.split('.so')[0])
+            linked.add(link)
 
-	for dep in lib_map.keys() :
-		if dep in lib_map :
-			m = lib_map[dep]
-			if m not in visited :
-				visited.add(m)
-				dnam = os.path.basename(dep)
-				traverse_so(m, dnam, f, depth+1, visited, linked, keep_suffix)
+    for dep in lib_map.keys() :
+        if dep in lib_map :
+            m = lib_map[dep]
+            if m not in visited :
+                visited.add(m)
+                dnam = os.path.basename(dep)
+                traverse_so(m, dnam, f, depth+1, visited, linked, keep_suffix)
 
+
+def sotrace(target: str, out: str):
+    """Cli tools to create the dependecies graph 
+    
+
+    Args:
+        target (str): binary file or Process ID
+        out (str): graph file output with the .dot extention
+    """
+    if target is None or out is None:
+        print(f"Usage:    {sys.argv[0]} libfoo.so out.dot")
+        print(f"Alt Usage: {sys.argv[0]} <PID> out.dot")
+    
+    target = str(target)
+    nam = os.path.basename(target)
+    with open(out, "w") as f:
+        f.write("digraph G {\n")
+        f.write("  rankdir = LR;\n")
+
+        linked = set()
+        visited = set()
+
+        if target.isnumeric() :
+            keep_suffix = False
+            trace_pid(target, f, visited, linked, keep_suffix)
+        else:
+            keep_suffix = True
+            traverse_so(target, nam, f, 0, visited, linked, keep_suffix)
+
+        for link in linked :
+            f.write(f'"{link[0]}" -> "{link[1]}"\n')
+
+        f.write("}\n")
+	
+     
 # Main entry point
 if __name__ == '__main__' :
-
-	if len(sys.argv) != 3 :
-		print("Usage:    ", sys.argv[0], "libfoo.so out.dot")
-		print("Alt Usage:", sys.argv[0], "<PID> out.dot")
-		sys.exit(1)
-
-	tgt = sys.argv[1]
-	out = sys.argv[2]
-	nam = os.path.basename(tgt)
-
-	f = open(out, "w")
-	f.write("digraph G {\n")
-	f.write("  rankdir = LR;\n")
-
-	linked = set()
-	visited = set()
-
-	if tgt.isnumeric() :
-		keep_suffix = False
-		trace_pid(tgt, f, visited, linked, keep_suffix)
-	else :
-		keep_suffix = True
-		traverse_so(tgt, nam, f, 0, visited, linked, keep_suffix)
-
-	for link in linked :
-		f.write('"' + link[0] + '" -> "' + link[1] + '"\n')
-
-	f.write("}\n")
-	f.close()
+    fire.Fire(sotrace)
